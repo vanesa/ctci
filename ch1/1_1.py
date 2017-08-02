87d061f2294ae03ede9ab168bb981bdb7b2aec5b
Vanesa <mail@vanesaortiz.com>
2017-08-02 15:59:05 -0700

    add implementation of ex 1_1 with datastructure

--- ch1/1_1.py
+++ ch1/1_1.py
@@ -3,9 +3,26 @@ Cracking the Code Interview Excercises
 
 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
+>>> isUnique('asdfghjkl')
+True
 
+>>> isUnique('asdfghajks')
+False
 
 """
 
+""" With datastructure Dictionary """
+def isUnique(word):
+	word_dict = {}
+	for letter in word:
+		if letter in word_dict:
+			return False
+		word_dict[letter] = True
+	return True
 
 
+if __name__ == "__main__":
+	from doctest import testmod
+	if testmod().failed == 0:
+		app.run(debug==True)
+		print 'All the tests passed. Awesome!'
