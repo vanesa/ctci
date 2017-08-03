b5e400adf2382a67079eb9f3a33d9d3aaa585705
Vanesa <mail@vanesaortiz.com>
2017-08-03 16:44:32 -0700

    add unittest

--- ch1/1_7.py
+++ ch1/1_7.py
@@ -9,6 +9,8 @@ write a method to rotate the image by 90 degrees. Can you do this in place?
 
 """
 
+import unittest
+
 def rotatematrix(matrix):
 
 	matrix90 = []
@@ -22,11 +24,47 @@ def rotatematrix(matrix):
 		row90 = []
 		num += 1
 
-	print matrix90
+	return matrix90
 
+class Test(unittest.TestCase):
+	data = [
+		([
+			[0, 0, 0, 0],
+			[1, 1, 1, 1],
+			[1, 1, 1, 1],
+			[0, 0, 0, 0]
+		], [
+			[0, 1, 1, 0],
+			[0, 1, 1, 0], 
+			[0, 1, 1, 0], 
+			[0, 1, 1, 0]
+		]), ([
+			[1, 0, 0],
+			[1, 1, 1],
+			[1, 1, 1]
+		], [
+			[1, 1, 1],
+			[1, 1, 0],
+			[1, 1, 0]
+		]), ([
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20],
+            [21, 22, 23, 24, 25]
+        ], [
+            [21, 16, 11, 6, 1],
+            [22, 17, 12, 7, 2],
+            [23, 18, 13, 8, 3],
+            [24, 19, 14, 9, 4],
+            [25, 20, 15, 10, 5]
+        ])
+	]
+
+	def test_rotatematrix(self):
+		for [test_matrix, expected] in self.data:
+			actual = rotatematrix(test_matrix)
+			self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
-	from doctest import testmod
-	if testmod.failed() == 0:
-		# High five!
-		app.run(debug=True)
+	unittest.main()
