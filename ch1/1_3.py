0068a607c58ec7d73ab975e2c5b233e5c58c38e5
Vanesa <mail@vanesaortiz.com>
2017-08-03 13:31:54 -0700

    add unittest

--- ch1/1_3.py
+++ ch1/1_3.py
@@ -5,14 +5,18 @@ URLify: Write a mathod to replace all spaces in a string with with '%20'.
 You may assume that the string has sufficient space at the end to hold the additional characters, and that
 you are given the 'true' length of the string.
 
->>> urlify('Mr John Smith', 13)
+>>> urlify('Mr John Smith     ', 13)
 'Mr%20John%20Smith'
 
 """
 
+import unittest
+
 def urlify(phrase, num):
 
 	urlified = ''
+	phrase = phrase.strip()
+
 	for character in phrase:
 		if character == ' ':
 			urlified += '%20'
@@ -21,8 +25,26 @@ def urlify(phrase, num):
 
 	return urlified
 
+
+class Test(unittest.TestCase):
+	""" Test Cases """
+
+	data = [
+		('Hey how is everything going      ', 27, 'Hey%20how%20is%20everything%20going'),
+		('Mr John Smith    ', 13, 'Mr%20John%20Smith')
+
+	]
+
+	def test_urlify(self):
+		for [test_string, length, expected] in self.data:
+			actual = urlify(test_string, length)
+			self.assertEqual(actual, expected)
+
 if __name__ == '__main__':
-	from doctest import testmod
-	if testmod.failed() == 0:
-		# Great!
-		app.run(debug=True)
+	unittest.main()
+
+# if __name__ == '__main__':
+# 	from doctest import testmod
+# 	if testmod.failed() == 0:
+# 		# Great!
+# 		app.run(debug=True)
